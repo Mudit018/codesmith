0a130f22a9e76c19aac1fa2030f72220348782c4
Shashwat <shashwatmittal.india@gmail.com>
2024-03-24 22:32:55 +0530

    fix colour of layout for api testing

--- src/renderer/src/components/Api_Testing/http/Layout/Layout.jsx
+++ src/renderer/src/components/Api_Testing/http/Layout/Layout.jsx
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <>
       <main>
-        <div className='flex max-w-6xl min-h-screen pt-[86px] mx-auto px-5 bg-gray-700 '>
+        <div className='flex max-w-6xl min-h-screen pt-[86px] mx-auto px-5 bg-gray-800 '>
           <div className='mx-auto w-full'>{children}</div>
         </div>
       </main>
