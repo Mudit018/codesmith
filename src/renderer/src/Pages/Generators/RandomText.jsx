fd0ab1a9c50046abcc9a2bc7e13d6b37bb508bed
Shashwat <shashwatmittal.india@gmail.com>
2024-03-24 22:57:49 +0530

    implement random text generation

--- src/renderer/src/Pages/Generators/RandomText.jsx
+++ src/renderer/src/Pages/Generators/RandomText.jsx
@@ -1,69 +1,67 @@
-import PageHeading from '../../components/PageHeading/PageHeading'
-import Card from '../../components/Card/Card'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-const RandomText = () => {
+import React, { useState } from 'react';
+import PageHeading from '../../components/PageHeading/PageHeading';
+import Card from '../../components/Card/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
+
+const RandomTextGenerator = () => {
+  const [textLength, setTextLength] = useState(100);
+  const [generatedText, setGeneratedText] = useState('');
+
+  const generateRandomText = () => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let text = '';
+    for (let i = 0; i < textLength; i++) {
+      text += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    setGeneratedText(text);
+  };
+
+  const copyText = () => {
+    navigator.clipboard.writeText(generatedText);
+    alert('Text copied to clipboard!');
+  };
+
   return (
-    <div className="w-[100%] flex flex-wrap justify-between gap-8 lg:gap-0">
-      <PageHeading>RandomText</PageHeading>
-      <Card tailStyle={'w-[100%] lg:w-[65%]'}>
-        <h5 className="p-3 px-6 border-b border-gray-700">Input</h5>
-        <form action="" className="p-4 px-6 ">
-          <div className="relative inline-flex flex-col w-[100%] mb-4">
+    <div className="w-full flex flex-wrap justify-between gap-8 lg:gap-0">
+      <Card tailStyle={'w-[100%]'}>
+        <div className="p-4 px-6">
+          <PageHeading>Random Text Generator</PageHeading>
+          <div className="flex items-center justify-between mt-4">
+            <div className="flex gap-4 w-full">
+              <input
+                type="number"
+                className="bg-gray-700 p-3 pl-4 pr-10 focus:outline-none border-transparent border focus:border-blue-600 focus:border w-1/3"
+                value={textLength}
+                onChange={(e) => setTextLength(e.target.value)}
+              />
+              <button
+                className="bg-blue-700 text-white py-3 px-6 rounded-md cursor-pointer"
+                onClick={generateRandomText}
+              >
+                Generate Text
+              </button>
+            </div>
+          </div>
+          <div className="relative mt-4">
             <textarea
-              id="Text"
-              placeholder="Text"
-              className="bg-gray-700 p-3 pl-10 focus:outline-none border-transparent border focus:border-blue-600 focus:border resize-none h-48"
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque posuere
-              fermentum urna, eu condimentum mauris tempus ut. Donec fermentum blandit aliquet
-            </textarea>
-            <FontAwesomeIcon icon={faPenToSquare} className="absolute top-11 left-4" />
-          </div>
-          <div className="ml-auto mr-2 w-fit flex gap-4">
-            <input
-              className="border border-gray-700 py-2 px-4 cursor-pointer rounded-md"
-              type="reset"
-              value={'Reset'}
+              id="generatedText"
+              className="bg-gray-700 p-3 pl-10 focus:outline-none border-transparent border focus:border-blue-600 focus:border resize-none h-48 w-full"
+              value={generatedText}
+              readOnly
             />
-            <input
-              className="border border-gray-700 py-2 px-4 cursor-pointer rounded-md bg-blue-700"
-              type="submit"
-              value={'Submit'}
+            <FontAwesomeIcon
+              icon={faCopy}
+              className="absolute top-3 right-4 cursor-pointer text-gray-300"
+              onClick={copyText}
             />
           </div>
-        </form>
-      </Card>
-      <Card tailStyle={'w-[100%] lg:w-[33%] h-fit'}>
-        <h5 className="p-3 px-6 border-b border-gray-700">Output</h5>
-        <form action="" className="p-4 px-6 ">
-          <div className="relative inline-flex flex-col w-[100%] mb-4">
-            <textarea
-              id="Text"
-              placeholder="Text"
-              className="bg-gray-700 p-3 pl-10 focus:outline-none border-transparent border focus:border-blue-600 focus:border resize-none h-48"
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque posuere
-              fermentum urna, eu condimentum mauris tempus ut. Donec fermentum blandit aliquet
-            </textarea>
-            <FontAwesomeIcon icon={faPenToSquare} className="absolute top-11 left-4" />
-          </div>
-          <div className="ml-auto mr-2 w-fit flex gap-4">
-            <input
-              className="border border-gray-700 py-2 px-4 cursor-pointer rounded-md"
-              type="reset"
-              value={'Reset'}
-            />
-            <input
-              className="border border-gray-700 py-2 px-4 cursor-pointer rounded-md bg-blue-700"
-              type="submit"
-              value={'Submit'}
-            />
-          </div>
-        </form>
+        </div>
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default RandomText
+export default RandomTextGenerator;
+
+
