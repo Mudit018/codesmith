83fe5585afd08e6bcb87907888cbdf42b89b5625
Shashwat <shashwatmittal.india@gmail.com>
2024-03-25 01:58:57 +0530

    implement uuid generator

--- src/renderer/src/Pages/Generators/uuid.jsx
+++ src/renderer/src/Pages/Generators/uuid.jsx
@@ -1,69 +1,60 @@
-import PageHeading from '../../components/PageHeading/PageHeading'
-import Card from '../../components/Card/Card'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-const Uuid = () => {
-  return (
-    <div className="w-[100%] flex flex-wrap justify-between gap-8 lg:gap-0">
-      <PageHeading>Uuid</PageHeading>
-      <Card tailStyle={'w-[100%] lg:w-[65%]'}>
-        <h5 className="p-3 px-6 border-b border-gray-700">Input</h5>
-        <form action="" className="p-4 px-6 ">
-          <div className="relative inline-flex flex-col w-[100%] mb-4">
-            <textarea
-              id="Text"
-              placeholder="Text"
-              className="bg-gray-700 p-3 pl-10 focus:outline-none border-transparent border focus:border-blue-600 focus:border resize-none h-48"
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque posuere
-              fermentum urna, eu condimentum mauris tempus ut. Donec fermentum blandit aliquet
-            </textarea>
-            <FontAwesomeIcon icon={faPenToSquare} className="absolute top-11 left-4" />
-          </div>
-          <div className="ml-auto mr-2 w-fit flex gap-4">
-            <input
-              className="border border-gray-700 py-2 px-4 cursor-pointer rounded-md"
-              type="reset"
-              value={'Reset'}
-            />
-            <input
-              className="border border-gray-700 py-2 px-4 cursor-pointer rounded-md bg-blue-700"
-              type="submit"
-              value={'Submit'}
-            />
-          </div>
-        </form>
-      </Card>
-      <Card tailStyle={'w-[100%] lg:w-[33%] h-fit'}>
-        <h5 className="p-3 px-6 border-b border-gray-700">Output</h5>
-        <form action="" className="p-4 px-6 ">
-          <div className="relative inline-flex flex-col w-[100%] mb-4">
-            <textarea
-              id="Text"
-              placeholder="Text"
-              className="bg-gray-700 p-3 pl-10 focus:outline-none border-transparent border focus:border-blue-600 focus:border resize-none h-48"
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque posuere
-              fermentum urna, eu condimentum mauris tempus ut. Donec fermentum blandit aliquet
-            </textarea>
-            <FontAwesomeIcon icon={faPenToSquare} className="absolute top-11 left-4" />
-          </div>
-          <div className="ml-auto mr-2 w-fit flex gap-4">
-            <input
-              className="border border-gray-700 py-2 px-4 cursor-pointer rounded-md"
-              type="reset"
-              value={'Reset'}
-            />
-            <input
-              className="border border-gray-700 py-2 px-4 cursor-pointer rounded-md bg-blue-700"
-              type="submit"
-              value={'Submit'}
-            />
-          </div>
-        </form>
-      </Card>
-    </div>
-  )
-}
+import React, { useState } from 'react';
+import PageHeading from '../../components/PageHeading/PageHeading';
+import Card from '../../components/Card/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
 
-export default Uuid
+const UUIDGenerator = () => {
+  const [uuid, setUUID] = useState('');
+  const [showPrompt, setShowPrompt] = useState(false);
+
+  const generateUUID = () => {
+    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      const r = Math.random() * 16 | 0;
+      const v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+    setUUID(uuid);
+  };
+
+  const copyUUID = () => {
+    navigator.clipboard.writeText(uuid);
+    setShowPrompt(true);
+    setTimeout(() => {
+      setShowPrompt(false);
+    }, 3000);
+  };
+
+  return (
+    <div className="w-full flex justify-center items-center h-screen">
+      <Card tailStyle={'w-[50%]'}>
+        <h5 className="p-3 px-6 border-b border-gray-700">UUID Generator</h5>
+        <div className="p-4 px-6">
+          <div className="flex justify-between items-center mb-4">
+            <p className="text-white font-semibold">{uuid}</p>
+            <FontAwesomeIcon
+              icon={faCopy}
+              className="cursor-pointer text-gray-300"
+              onClick={copyUUID}
+            />
+          </div>
+          <div className="flex justify-center">
+            <button
+              className="bg-blue-700 text-white py-2 px-4 rounded-md"
+              onClick={generateUUID}
+            >
+              Generate UUID
+            </button>
+          </div>
+        </div>
+      </Card>
+      {showPrompt && (
+        <div className="fixed bottom-0 right-0 mb-4 mr-4 bg-gray-900 text-white p-3 rounded-md">
+          UUID copied to clipboard!
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UUIDGenerator;
