a7626eff6c2acc47f2010484ab47b9b36998d243
Shashwat <shashwatmittal.india@gmail.com>
2024-03-31 22:51:15 +0530

    implement regex

--- src/renderer/src/Pages/Text/regex.jsx
+++ src/renderer/src/Pages/Text/regex.jsx
@@ -1,69 +1,71 @@
-import PageHeading from '../../components/PageHeading/PageHeading'
-import Card from '../../components/Card/Card'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { useState, useEffect } from 'react';
+import PageHeading from '../../components/PageHeading/PageHeading';
+import Card from '../../components/Card/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare, faCopy } from '@fortawesome/free-solid-svg-icons';
+
 const Regex = () => {
+  const [regexInput, setRegexInput] = useState('');
+  const [inputText, setInputText] = useState('');
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    testRegex();
+  }, [regexInput, inputText]);
+
+  const testRegex = () => {
+    const regex = new RegExp(regexInput);
+    const matches = inputText.match(regex);
+    setMatches(matches);
+  };
+
   return (
     <div className="w-[100%] flex flex-wrap justify-between gap-8 lg:gap-0">
-      <PageHeading>Regex</PageHeading>
-      <Card tailStyle={'w-[100%] lg:w-[65%]'}>
-        <h5 className="p-3 px-6 border-b border-gray-700">Input</h5>
+      <PageHeading>Regex Tester</PageHeading>
+      <Card tailStyle={'w-[100%] lg:w-[35%]'}>
+        <h5 className="p-3 px-6 border-b border-gray-700">Regex Input</h5>
         <form action="" className="p-4 px-6 ">
           <div className="relative inline-flex flex-col w-[100%] mb-4">
             <textarea
-              id="Text"
-              placeholder="Text"
+              id="Regex"
+              placeholder="Enter regex here"
               className="bg-gray-700 p-3 pl-10 focus:outline-none border-transparent border focus:border-blue-600 focus:border resize-none h-48"
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque posuere
-              fermentum urna, eu condimentum mauris tempus ut. Donec fermentum blandit aliquet
-            </textarea>
+              value={regexInput}
+              onChange={(e) => setRegexInput(e.target.value)}
+            />
             <FontAwesomeIcon icon={faPenToSquare} className="absolute top-11 left-4" />
-          </div>
-          <div className="ml-auto mr-2 w-fit flex gap-4">
-            <input
-              className="border border-gray-700 py-2 px-4 cursor-pointer rounded-md"
-              type="reset"
-              value={'Reset'}
-            />
-            <input
-              className="border border-gray-700 py-2 px-4 cursor-pointer rounded-md bg-blue-700"
-              type="submit"
-              value={'Submit'}
-            />
           </div>
         </form>
       </Card>
-      <Card tailStyle={'w-[100%] lg:w-[33%] h-fit'}>
-        <h5 className="p-3 px-6 border-b border-gray-700">Output</h5>
+      <Card tailStyle={'w-[100%] lg:w-[35%]'}>
+        <h5 className="p-3 px-6 border-b border-gray-700">Input Text</h5>
         <form action="" className="p-4 px-6 ">
           <div className="relative inline-flex flex-col w-[100%] mb-4">
             <textarea
               id="Text"
-              placeholder="Text"
+              placeholder="Enter text to test"
               className="bg-gray-700 p-3 pl-10 focus:outline-none border-transparent border focus:border-blue-600 focus:border resize-none h-48"
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque posuere
-              fermentum urna, eu condimentum mauris tempus ut. Donec fermentum blandit aliquet
-            </textarea>
+              value={inputText}
+              onChange={(e) => setInputText(e.target.value)}
+            />
             <FontAwesomeIcon icon={faPenToSquare} className="absolute top-11 left-4" />
           </div>
-          <div className="ml-auto mr-2 w-fit flex gap-4">
-            <input
-              className="border border-gray-700 py-2 px-4 cursor-pointer rounded-md"
-              type="reset"
-              value={'Reset'}
-            />
-            <input
-              className="border border-gray-700 py-2 px-4 cursor-pointer rounded-md bg-blue-700"
-              type="submit"
-              value={'Submit'}
-            />
-          </div>
         </form>
+      </Card>
+      <Card tailStyle={'w-[100%] lg:w-[30%]'}>
+        <h5 className="p-3 px-6 border-b border-gray-700">Matches</h5>
+        <div className="p-4 px-6 ">
+          <textarea
+            id="Matches"
+            placeholder="Matches will appear here"
+            className="bg-gray-700 p-3 pl-10 focus:outline-none border-transparent border focus:border-blue-600 focus:border resize-none h-48"
+            readOnly
+            value={matches?.join('\n') || 'No matches found'}
+          />
+        </div>
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default Regex
+export default Regex;
