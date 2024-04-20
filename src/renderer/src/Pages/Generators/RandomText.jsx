3b042745b57a5392bf3fdf88faea1b52a1d238cb
Pankaj Kumar <76695979+pankaj1707k@users.noreply.github.com>
2024-04-20 14:33:26 +0530

    feat: lorem ipsum generator

--- src/renderer/src/Pages/Generators/RandomText.jsx
+++ src/renderer/src/Pages/Generators/RandomText.jsx
@@ -1,26 +1,27 @@
-import { useState } from 'react';
-import PageHeading from '../../components/PageHeading/PageHeading';
-import Card from '../../components/Card/Card';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react'
+import PageHeading from '../../components/PageHeading/PageHeading'
+import Card from '../../components/Card/Card'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopy } from '@fortawesome/free-solid-svg-icons'
+import { generateLoremIpsum } from '../../../../main/generators/randomText'
 
 const RandomTextGenerator = () => {
-  const [textLength, setTextLength] = useState(100);
-  const [generatedText, setGeneratedText] = useState('');
+  const [wordsPerSentence, setWordsPerSentence] = useState(10)
+  const [sentencePerPara, setSentencePerPara] = useState(3)
+  const [paragraph, setParagraphs] = useState(1)
+  const [startWithLoremIpsum, setStartWithLoremIpsum] = useState(true)
+  const [generatedText, setGeneratedText] = useState('')
 
   const generateRandomText = () => {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let text = '';
-    for (let i = 0; i < textLength; i++) {
-      text += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    setGeneratedText(text);
-  };
+    setGeneratedText(
+      generateLoremIpsum(paragraph, sentencePerPara, wordsPerSentence, startWithLoremIpsum)
+    )
+  }
 
   const copyText = () => {
-    navigator.clipboard.writeText(generatedText);
-    alert('Text copied to clipboard!');
-  };
+    navigator.clipboard.writeText(generatedText)
+    alert('Text copied to clipboard!')
+  }
 
   return (
     <div className="w-full flex flex-wrap justify-between gap-8 lg:gap-0">
@@ -29,12 +30,53 @@ const RandomTextGenerator = () => {
           <PageHeading>Random Text Generator</PageHeading>
           <div className="flex items-center justify-between mt-4">
             <div className="flex gap-4 w-full">
-              <input
-                type="number"
-                className="bg-gray-700 p-3 pl-4 pr-10 focus:outline-none border-transparent border focus:border-blue-600 focus:border w-1/3"
-                value={textLength}
-                onChange={(e) => setTextLength(e.target.value)}
-              />
+              <label>
+                Words per sentence
+                <input
+                  type="number"
+                  className="bg-gray-700 p-3 pl-4 focus:outline-none border-transparent border focus:border-blue-600 focus:border w-1/3"
+                  value={wordsPerSentence}
+                  onChange={(e) => setWordsPerSentence(e.target.value)}
+                />
+              </label>
+              <label>
+                Sentences per paragraph
+                <input
+                  type="number"
+                  className="bg-gray-700 p-3 pl-4 focus:outline-none border-transparent border focus:border-blue-600 focus:border w-1/3"
+                  value={sentencePerPara}
+                  onChange={(e) => setSentencePerPara(e.target.value)}
+                />
+              </label>
+              <label>
+                Paragraphs
+                <input
+                  type="number"
+                  className="bg-gray-700 p-3 pl-4 focus:outline-none border-transparent border focus:border-blue-600 focus:border w-1/3"
+                  value={paragraph}
+                  onChange={(e) => setParagraphs(e.target.value)}
+                />
+              </label>
+              <label>
+                Yes
+                <input
+                  type="radio"
+                  className="bg-gray-700 p-3 pl-4 focus:outline-none border-transparent border focus:border-blue-600 focus:border w-1/3"
+                  // value={true}
+                  checked={startWithLoremIpsum === true}
+                  onChange={(_) => setStartWithLoremIpsum(true)}
+                />
+              </label>
+              <label>
+                No
+                <input
+                  type="radio"
+                  className="bg-gray-700 p-3 pl-4 focus:outline-none border-transparent border focus:border-blue-600 focus:border w-1/3"
+                  // value={false}
+                  checked={startWithLoremIpsum === false}
+                  onChange={(_) => setStartWithLoremIpsum(false)}
+                />
+              </label>
               <button
                 className="bg-blue-700 text-white py-3 px-6 rounded-md cursor-pointer"
                 onClick={generateRandomText}
@@ -46,7 +88,7 @@ const RandomTextGenerator = () => {
           <div className="relative mt-4">
             <textarea
               id="generatedText"
-              className="bg-gray-700 p-3 pl-10 focus:outline-none border-transparent border focus:border-blue-600 focus:border resize-none h-48 w-full"
+              className="bg-gray-700 p-3 focus:outline-none border-transparent border focus:border-blue-600 focus:border resize-none h-48 w-full"
               value={generatedText}
               readOnly
             />
@@ -59,9 +101,7 @@ const RandomTextGenerator = () => {
         </div>
       </Card>
     </div>
-  );
-};
+  )
+}
 
-export default RandomTextGenerator;
-
-
+export default RandomTextGenerator
